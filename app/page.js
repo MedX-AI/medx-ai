075fc9e9ccec674b140efc02301cac52e7644996
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-10-01 00:24:37 +0530

    navbar styles

--- app/page.js
+++ app/page.js
@@ -2,21 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="m-5 flex justify-center flex-col">
-
+    <div className="my-5 backdrop-blur-sm mx-10 py-2 px-10 bg-indigo-950 flex justify-center flex-col rounded-lg">
       <div className="flex justify-between items-center">
-        <div className="flex">
-          <h1 className="text-8xl">MEDX</h1>
-          <h1 className="text-4xl"><sup>AI</sup></h1>
+        <div className="flex items-start">
+          <h1 className="text-6xl">MEDX</h1>
+          <h1 className="text-xl">AI</h1>
         </div>
         
-        <nav className="flex space-x-4 items-center">
-          <a href="#about" className="text-l">WHAT WE DO</a>
-          <a href="#about" className="text-l">THE PROBLEM</a>
-          <a href="#about" className="text-l">THE SOLUTION</a>
-          <a href="#about" className="text-l">ABOUT</a>
+        <nav className="flex space-x-6 items-center">
+          <a href="#about" className="text-sm">WHAT WE DO</a>
+          <a href="#about" className="text-sm">THE PROBLEM</a>
+          <a href="#about" className="text-sm">THE SOLUTION</a>
+          <a href="#about" className="text-sm">ABOUT</a>
         </nav>
-          <a href="#demo" className="text-xl">TRY OUT DEMO</a>
+          <a href="#demo" className="text-sm p-3.5 bg-gradient-to-r from-cyan-500 to-blue-500  rounded-full">TRY OUT DEMO</a>
       </div>
 
     </div>
